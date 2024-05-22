291650e7b5e914ced357b2ec2372b99fe3d9008d
Victor <vpsb888@gmail.com>
2024-05-23 00:25:17 +0200

    Fin del video 5
    no funcionan aun los endpoints

--- src/main/java/edu/daw/web/controllers/TareaController.java
+++ src/main/java/edu/daw/web/controllers/TareaController.java
@@ -2,12 +2,14 @@ package edu.daw.web.controllers;
 
 import edu.daw.persistence.entities.Tarea;
 import edu.daw.services.TareaService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tareas")
@@ -16,12 +18,40 @@ public class TareaController {
     private TareaService tareaService;
 
     @GetMapping
-    public List<Tarea> listAll(){
-        return this.tareaService.getTareas();
+    public ResponseEntity<List<Tarea>> listAll() {
+
+        return ResponseEntity.ok(this.tareaService.getTareas());
     }
 
+    @GetMapping("/{idTarea}")
+    public ResponseEntity<Tarea> findOne(@PathVariable int idTarea) {
+        Optional<Tarea> tarea = this.tareaService.getTarea(idTarea);
+        if (tarea.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(tarea.get());
+    }
 
+    @PostMapping
+    public ResponseEntity<Tarea> create(@RequestBody Tarea tarea) {
+        return new ResponseEntity<Tarea>(this.tareaService.crearTarea(tarea), HttpStatus.CREATED);
+    }
 
+    @PutMapping
+    public ResponseEntity<Tarea> update(int idTarea, @RequestBody Tarea tarea) {
+        if (idTarea == tarea.getId()) {
+            if (this.tareaService.getTarea(idTarea).isPresent()) {
+                return ResponseEntity.ok(this.tareaService.actualizarTarea(tarea));
+            }
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
+    public ResponseEntity<Tarea> delete(int idTarea){
+        if (this.tareaService.borrarTarea(idTarea)){
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
